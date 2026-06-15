72369205fd412fbefb2a3eb7ca4e2ddc9b901c84
hizok <hizok@host.docker.internal>
2026-06-16 02:26:34 +0900

    single_insertion 回答

--- PaizaTest/src/main/java/query_primer/single_insertion/SingleInsertionPrinter.java
+++ PaizaTest/src/main/java/query_primer/single_insertion/SingleInsertionPrinter.java
@@ -2,20 +2,28 @@ package query_primer.single_insertion;
 
 import java.io.PrintStream;
 
-class SingleInsertionPrinter {
+import valueobjects.ConstrainedInteger;
+
+final class SingleInsertionPrinter {
     private final PrintStream out;
 
     SingleInsertionPrinter(final PrintStream out) {
         this.out = out;
     }
 
-    void printSingleInsertion(final String targetWord, final String[] checkLines) {
-        for (final String line : checkLines) {
-            if (line.contains(targetWord))
-                out.println("Yes");
-            else {
-                out.println("No");
-            }
+    void printSingleInsertion(final ConstrainedInteger order, ConstrainedInteger targetNumber, final ConstrainedInteger[] arrays) {
+        final int[] results = new int[arrays.length + 1];
+        
+        for (int i = 0; i < order.getValue(); i++) {
+            results[i] = arrays[i].getValue();
+        }
+        results[order.getValue()] = targetNumber.getValue();
+        for(int j = order.getValue() + 1; j < arrays.length + 1; j++) {
+            results[j] = arrays[j - 1].getValue();
+        }
+        
+        for (final int result : results) {
+            out.println(result);
         }
     }
 }
